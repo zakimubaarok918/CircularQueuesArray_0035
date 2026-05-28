0eb350cd475e3f8496e6fd89f67477dcf3b93bcf
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-28 15:27:16 +0700

    Fix queue overflow check in Insert method of Queues class

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -23,4 +23,8 @@ private:
         cout << endl;
 
         // cek apakah antrian penuh
-        
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
