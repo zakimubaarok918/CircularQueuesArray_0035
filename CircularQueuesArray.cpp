3fbfea73aa4593861b6309b11f9ddf6eb32934c3
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-28 15:28:48 +0700

    Refactor Insert method to improve queue empty check in Queues class

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -28,3 +28,13 @@ private:
             cout << "\nQueue overflow\n";
             return;
         }
+
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else
+        {
+            // jika rear berada di posisi terakhir array, kembali ke awal array
