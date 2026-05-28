9a27bdec77efec1812a5cc1a03525abd7b18181f
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-28 15:37:28 +0700

    Fix display method to handle circular queue iteration correctly in Queues class

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -88,3 +88,13 @@ private:
         cout << "\nElements in the queue are....\n";
 
         // jika FRONT <= REAR, iterasi dari FRONT hingga REAR
+        if (FRONT_Position <= REAR_Position)
+        {
+            while (FRONT_Position <= REAR_Position)
+            {
+                cout << queue_array[FRONT_Position] << "  ";
+                FRONT_Position++;
+            }
+            cout << endl;
+        }
+        else
