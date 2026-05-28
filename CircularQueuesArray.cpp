ffb74db7fc893b26a0db4e039c0fcc46bb39d766
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-28 15:24:33 +0700

    Add constructor and Insert method to Queues class

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -7,3 +7,14 @@ private:
     static const int max = 3;
     int FRONT, REAR;
     int queue_array[max];
+
+    public:
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    void Insert()
+    {
+        int num;
