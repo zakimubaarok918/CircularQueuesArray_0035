3eeb1c7693f4b4fc7991d079d1c00c6cd173aca5
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-28 15:33:22 +0700

    Fix remove method to reset FRONT and REAR when the last element is deleted in Queues class

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -58,3 +58,13 @@ private:
 
         // cek jika antrian hanya memiliki satu elemen
         if (FRONT == REAR)
+       {    
+            FRONT = -1;
+            REAR = -1;
+        }
+        else
+        {
+            // jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
