8959c4c7eb1c0a4550bf6d108116bbe23ab9c615
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-28 15:39:03 +0700

    Fix display method to handle circular queue iteration when FRONT > REAR in Queues class

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -98,3 +98,13 @@ private:
             cout << endl;
         }
         else
+        {
+            // jika FRONT > REAR, iterasi dari FRONT hingga akhir array
+            while (FRONT_Position <= max - 1)
+            {
+                cout << queue_array[FRONT_Position] << "  ";
+                FRONT_Position++;
+            }
+
+            FRONT_Position = 0;
+            // iterasi dari awal array hingga REAR
